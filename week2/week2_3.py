57633151a13640a98359fdd12cc5d1ef3730d041
YoojLee <daebaq27@gmail.com>
2022-05-05 22:51:15 +0900

    advanced solution added

--- week2/week2_3.py
+++ week2/week2_3.py
@@ -1,18 +1,20 @@
+# 빙고 (lv3)
+
 def solution(board, nums):
     """
     set 자료형은 해시테이블로 구현되어 있기 때문에 O(1) 시간에 탐색이 가능함.
     
     원래는 list 자료형인 nums에서 board[row][col]을 찾았기 때문에, O(n^3)의 시간복잡도를 가졌음.
     그러나, nums를 set 자료형으로 변경함으로써 O(n^2)의 시간복잡도를 가지며 코드 효율 개선.
-    
-    zip(*board) -> 이런 식으로 하면 column-wise하게 묶을 수 있음.
     """
     answer = 0
     n = len(board)
+    nums = set(nums)
     
-    """
+    
     # before
     ## bottleneck in this code : O(n^3)
+    """
     for row in range(n):
         for col in range(n):
             if board[row][col] in nums:
@@ -21,7 +23,7 @@ def solution(board, nums):
     
     for row in range(n):
         for col in range(n):
-            if board[row][col] in set(nums):
+            if board[row][col] in nums:
                 board[row][col] = 0
     
     
@@ -55,13 +57,30 @@ def solution(board, nums):
             break
     else:
         answer += 1
-                         
-                
+     
+    return answer
+
+
+def solution2(board, nums):
     """
-    # 각 빙고 방향에 대해서 다 룰을 적용해서 빙고를 찾아냄.
-    answer += len([i for i in board if sum(i) == 0]) # horizontal
-    answer += len([i for i in zip(*board) if sum(i) == 0]) # vertical (이렇게 하면 vertical이 되는 게 더 신기함)
-    answer += int(sum(board[i][i] for i in range(n)) == 0) # diagonal
-    answer += int(sum(board[n - 1 - i][i] for i in range(n)) == 0) # off-diagonal
-    """        
+    zip(*board) -> 이런 식으로 하면 column-wise하게 묶을 수 있음.
+    """
+    answer = 0
+    n = len(board)
+    nums = set(nums)
+    
+    for row in range(n):
+        for col in range(n):
+            if board[row][col] in nums:
+                board[row][col] = 0
+                
+	# horizontal (각 행을 탐색하면서 sum = 0인지 체크)
+    answer += len([i for i in board if sum(i)==0])
+    # vertical (각 열을 탐색하면서 sum = 0인지 체크)
+    answer += len([i for i in zip(*board) if sum(i)==0])
+    # diagonal (대각요소의 합이 0인지 체크)
+    answer += int(sum(board[i][i] for i in range(n))==0)
+    # off-diagonal
+    answer += int(sum(board[n-i-1][i] for i in range(n))==0)
+    
     return answer
