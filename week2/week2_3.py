8126f5c41654c47af3aa089d61455e86fb10ce06
YoojLee <daebaq27@gmail.com>
2022-05-05 22:17:26 +0900

    3. bingo (효율 개선)

--- week2/week2_3.py
+++ week2/week2_3.py
@@ -1,16 +1,29 @@
 def solution(board, nums):
     """
-    일단 nums 부분을 다 다른 값으로 대체해버리고 board 내에서 dfs 때려버리기..?
-    -> 무조건 한 방향으로만 탐색 루트를 정리를 해버려야할 것 같음 대신.
-    -> dfs 방법도 내일 해보기
+    set 자료형은 해시테이블로 구현되어 있기 때문에 O(1) 시간에 탐색이 가능함.
+    
+    원래는 list 자료형인 nums에서 board[row][col]을 찾았기 때문에, O(n^3)의 시간복잡도를 가졌음.
+    그러나, nums를 set 자료형으로 변경함으로써 O(n^2)의 시간복잡도를 가지며 코드 효율 개선.
+    
+    zip(*board) -> 이런 식으로 하면 column-wise하게 묶을 수 있음.
     """
     answer = 0
     n = len(board)
     
+    """
+    # before
+    ## bottleneck in this code : O(n^3)
     for row in range(n):
         for col in range(n):
             if board[row][col] in nums:
                 board[row][col] = 0
+    """
+    
+    for row in range(n):
+        for col in range(n):
+            if board[row][col] in set(nums):
+                board[row][col] = 0
+    
     
     # 오른쪽으로 이동 (horizontal 빙고)
     for i in range(n):
@@ -45,7 +58,7 @@ def solution(board, nums):
                          
                 
     """
-    # 각 빙고 방향에 대해서 다 룰베이스를 때려버린다.
+    # 각 빙고 방향에 대해서 다 룰을 적용해서 빙고를 찾아냄.
     answer += len([i for i in board if sum(i) == 0]) # horizontal
     answer += len([i for i in zip(*board) if sum(i) == 0]) # vertical (이렇게 하면 vertical이 되는 게 더 신기함)
     answer += int(sum(board[i][i] for i in range(n)) == 0) # diagonal
