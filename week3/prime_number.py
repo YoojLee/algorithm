d5c6eb2228a87658583de03b2e71b9d38262aa58
YoojLee <daebaq27@gmail.com>
2022-06-13 13:03:24 +0900

    세 소수의 합

--- week3/prime_number.py
+++ week3/prime_number.py
@@ -4,17 +4,16 @@ def solution(n):
     answer = 0
     prime_nums = [i for i in range(n+1)]
 
-    for i in range(2, int(math.sqrt(n))+1):
+    for i in range(2, int(math.sqrt(n))+1): # 2부터 n의 제곱근까지만 loop
         if prime_nums[i] == 0:
             continue
 
-        for j in range(i**2, n+1, i):
+        for j in range(i**2, n+1, i): # 그 안에서는 제곱부터 시작해서 i만큼 건너뛰기...흠
             prime_nums[j] = 0
         
     print([i for i in prime_nums[2:] if prime_nums[i]])
 
     return answer
-
 
 
 if __name__ == "__main__":
--- week3/week3_1.py
+++ week3/week3_1.py
@@ -3,29 +3,32 @@
 def solution(n):
     answer = 0
     
-    def primary_numbers(n):
+    def prime_numbers(n):
         """
-        에라토스테스트의 체 알고리즘을 기반으로!
+        에라토스테네스의 체 알고리즘을 기반으로!
         """
-        arr = [i+1 for i in range(1,n)]
+        arr = [i for i in range(n+1)]
         
-        for i in range(2,n+1):
-            for j in range(len(arr)):
-                if arr[j] == 0:
-                    continue
-                if arr[j] != i and arr[j] % i == 0:
-                    arr[j] = 0
+        for i in range(2,int(n**(1/2))+1):
+            if arr[i] == 0:
+                continue
+            for j in range(i**2, len(arr), i):
+                arr[j] = 0
                 
-        return [i for i in arr if i != 0]
+        return [i for i in arr[2:] if arr[i]]
     
-    primary = primary_numbers(n)
+    prime = prime_numbers(n)
     
-    for i in range(len(primary)):
-        for j in range(i,len(primary)):
-            for k in range(j,len(primary)):
+    for i in range(len(prime)):
+        for j in range(i,len(prime)):
+            for k in range(j,len(prime)):
                 if i == j or j == k or i == k:
                     continue
-                if primary[i]+primary[j]+primary[k] == n:
+                if prime[i]+prime[j]+prime[k] == n:
                     answer += 1
         
     return answer
+
+if __name__ == "__main__":
+    answer = solution(33)
+    print(answer)
